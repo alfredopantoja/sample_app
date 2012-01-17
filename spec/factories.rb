0da248d811afde5e3f64cf083cad70b211f2ce8b
Alfredo Pantoja <chololoy@gmail.com>
2012-01-17 03:43:14 -0600

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Michael J. Jordan"
+  user.email                 "mjj@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
